a600ad3f06c71104738368614dffce0935a299ed
Cryptogoldenergy <cfrylinck1997@gmail.com>
2022-11-06 20:56:12 +0200

    Removed stairs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Canvas } from "@react-three/fiber"
-import { Sky, PointerLockControls, KeyboardControls, useGLTF } from "@react-three/drei"
+import { Sky, PointerLockControls, KeyboardControls } from "@react-three/drei"
 import { Physics } from "@react-three/rapier"
 import { Ground } from "./components/Ground.js"
 import { Player } from "./components/Player.js"
 import './App.css';
-import stairUrl from "./assets/Stairs.glb"
 import { RigidBody } from "@react-three/rapier"
 import { TextFrameManager } from "./components/TextFrameManager.js"
 
 function App() {
-  const { nodes, materials } = useGLTF(stairUrl)
   return (
     <KeyboardControls
       map={[
@@ -26,8 +24,6 @@ function App() {
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
         <Physics gravity={[0, -30, 0]}>
           <RigidBody dispose={null}  position={[0, 1, -10]}  scale={[1, 1, 6]} type="fixed" colliders="hull">
-              <mesh geometry={nodes.Cube.geometry} material={materials.A} />
-              <mesh geometry={nodes.Lining.geometry} material={materials.LiningMat} />
               <TextFrameManager />
           </RigidBody>
           <Ground />
@@ -40,5 +36,3 @@ function App() {
 }
 
 export default App;
-
-useGLTF.preload("./assets/Stairs.glb")
