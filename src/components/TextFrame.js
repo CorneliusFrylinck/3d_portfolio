007ee8fd84a52159e7e15d0013189e8bf3289ae4
Cryptogoldenergy <cfrylinck1997@gmail.com>
2022-11-06 22:18:40 +0200

    Completed TextFrame management

--- src/components/TextFrame.js
+++ src/components/TextFrame.js
@@ -1,24 +1,35 @@
-
 import { Text } from "@react-three/drei"
+import { useState, useRef, useEffect } from "react"
 
 export function TextFrame(props) {
+  const [text,] = useState(props.text)
+  const [x,] = useState(props.x)
+  const [y,] = useState(props.y)
+  const [z,] = useState(props.z)
+  const [angle,] = useState(props.angle)
+  const group = useRef()
+
+  useEffect(() => {
+    group.current.rotateY(angle)
+  }, [angle])
+
   return (
-    <>
-      <mesh  position={[2, 0, -0.0011]}>
+    <group ref={group}>
+      <mesh position={[x, y, z - 0.0011]}>
         <boxGeometry args={[1.1, 1.1, 0.011]} />
         <meshStandardMaterial color={"blue"} />
       </mesh>
-      <mesh  position={[2, 0, -0.001]}>
+      <mesh position={[x, y, z - 0.001]}>
         <boxGeometry args={[1.03, 1.03, 0.011]} />
         <meshStandardMaterial color={"white"} />
       </mesh>
-      <mesh  position={[2, 0, 0]}>
+      <mesh position={[x, y, z]}>
         <boxGeometry args={[1, 1, 0.01]} />
         <meshStandardMaterial color={"#dedede"} />
-        <Text position={[0,0,0.008]} color={"#5c5c5c"}>
-          Words
+        <Text maxWidth={1} textAlign={"center"} position={[0,0,0.008]} color={"#5c5c5c"}>
+          {text}
         </Text>
       </mesh>
-    </>
+    </group>
   )
 }
--- src/components/TextFrameManager.js
+++ src/components/TextFrameManager.js
@@ -1,9 +1,25 @@
 import { TextFrame } from "./TextFrame";
 
 export function TextFrameManager(props) {
+  const frames = [
+    { 
+      text: "One of these things just doesn't belong here",
+      x: 0, y: 0, z: 0, angle: 0
+    },
+    { 
+      text: "One of these things just doesn't belong here",
+      x: 1.5, y: 0, z: -0.1, angle: -0.1
+    },
+    { 
+      text: "One of these things just doesn't belong here",
+      x: -1.5, y: 0, z: -0.1, angle: 0.1
+    }
+  ]
   return (
     <>
-      <TextFrame />
+      {frames.map(f => 
+        <TextFrame key={Math.random() * 10000000} text={f.text} x={f.x} y={f.y} z={f.z} angle={f.angle} />
+      )}
     </>
   )
 }
