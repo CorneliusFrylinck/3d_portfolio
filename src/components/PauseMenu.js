5960d091b551b8da6d5bcf2a1cc7123aa75803ae
Cryptogoldenergy <cfrylinck1997@gmail.com>
2022-11-15 08:23:09 +0200

    Updated app name and icon

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -11,9 +11,9 @@ console.log(gameStore.paused)
             {gameStore.paused && (
                 <div className="modal">
                     <h2>PAUSE MENU</h2>
-                    <div>To hide or show this menu, press 'h'............................................................................</div>
-                    <div>Movement is handled using the standard 'WASD' keys................................................</div>
-                    <div>To look around with your mouse, click on the screen first (if you haven't already)........</div>
+                    <div>To hide or show this menu, press 'h'.......................................................................</div><br/>
+                    <div>Movement is handled using the standard 'WASD' keys.............................................</div><br/>
+                    <div>To look around with your mouse, click on the screen first (if you haven't already)......</div><br/>
                     <div>When a canvas has an action, the action will be described on the canvas or above it</div>
                 </div>
             )}
