f3f7df9da6edb88d023cfcd94b75a58ee45f47a3
Cryptogoldenergy <cfrylinck1997@gmail.com>
2023-01-22 11:14:20 +0200

    Added Certification board

--- src/components/CanvasStandManager.js
+++ src/components/CanvasStandManager.js
@@ -4,46 +4,61 @@ export function CanvasStandManager(props) {
 
   return (
     <>
+      {/* SAVE THE DATE */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={8.015} y={-0.01} z={-10} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={3.16} angleZ={0} 
       />
+      {/* LUXIT */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={6.015} y={-0.01} z={-10} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={3.16} angleZ={0} 
       />
+      {/* OLD PORTFOLIO */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={10.015} y={-0.01} z={-10} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={3.16} angleZ={0} 
       />
+      {/* REACTIVITIES */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={12.015} y={-0.01} z={-10} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={3.16} angleZ={0} 
       />
+      {/* IBM CLOUD */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={11.515} y={-0.01} z={10.57} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={0} angleZ={0} 
       />
+      {/* AZ 900 */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={9.515} y={-0.01} z={10.57} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={0} angleZ={0} 
       />
+      {/* AZ 204 */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={7.515} y={-0.01} z={10.57} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={0} angleZ={0} 
       />
+      {/* UNIVERSITY */}
       <CanvasStand 
         key={Math.random() * 10000000}
-        x={13.015} y={-0.01} z={7.3} textScale={0.125} scale={[0.45, 0.25, 0.25]}
+        x={13.015} y={-0.01} z={8.38} textScale={0.125} scale={[0.45, 0.25, 0.25]}
         angleX={0} angleY={1.575} angleZ={0} 
       />
+      {/* CERTIFICATES */}
+      <CanvasStand 
+        key={Math.random() * 10000000}
+        x={13.015} y={-0.01} z={5.68} textScale={0.125} scale={[0.45, 0.25, 0.25]}
+        angleX={0} angleY={1.575} angleZ={0} 
+      />
+      {/* MATRIC */}
       <CanvasStand 
         key={Math.random() * 10000000}
         x={9.015} y={-0.01} z={3.1} textScale={0.125} scale={[0.45, 0.25, 0.25]}
